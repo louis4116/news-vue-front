8879abb0b1dc4f3ad86e2ae7e92ffdd482afd86b
unknown <qscasd7073@gmail.com>
2026-01-12 23:37:42 +0800

    update debounce

--- src/util/debounce.ts
+++ src/util/debounce.ts
@@ -1,4 +1,4 @@
-export function useDebounce<T extends unknown[]>(fn: (...args: T) => void, delay: number = 500) {
+export function debounce<T extends unknown[]>(fn: (...args: T) => void, delay: number = 500) {
   let timer: ReturnType<typeof setTimeout>
   return (...args: T) => {
     clearTimeout(timer)
